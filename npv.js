e391d042ebf327d10651f763279f302d0ba8c9d4
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2023-12-11 22:51:57 +0100

    Update npv.js

--- npv.js
+++ npv.js
@@ -1,1 +1,17 @@
 
+let intRate = 0.15
+let cashFlow = [1000,2000,3000,4000]
+
+
+function npv(i,t){
+  let arr = []
+ 
+    
+    for(let x = 1; x <= t; x++){
+        arr.push((1+i)**-x)
+    }
+    let fill = arr.map(x=> x.toFixed(4) )
+    return fill
+    
+}
+console.log(npv(0.15,5))
